52cfd9b7b2430eb058fe15ee5b90c6ee5e5bc8de
Андрей Баган <andrei.bagan2@mail.ru>
2022-01-29 19:24:50 +0300

    Добавьте файлы проекта.

---
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccessLibrary
+{
+    public class SampleDataAccess
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public SampleDataAccess(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            Thread.Sleep(3000);
+
+            return new List<EmployeeModel>()
+            {
+                new EmployeeModel{ FirstName = "Andei", LastName = "Bagan" },
+                new EmployeeModel{ FirstName = "Vadim", LastName = "Vadya" },
+                new EmployeeModel{ FirstName = "Denis", LastName = "Myasnik" }
+            };
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            await Task.Delay(3000);
+
+            return new List<EmployeeModel>()
+            {
+                new EmployeeModel{ FirstName = "Andei", LastName = "Bagan" },
+                new EmployeeModel{ FirstName = "Vadim", LastName = "Vadya" },
+                new EmployeeModel{ FirstName = "Denis", LastName = "Myasnik" }
+            };
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesCache()
+        {
+            var employees = _memoryCache.Get<List<EmployeeModel>>("employees");
+
+            if (employees is null)
+            {
+                employees = await GetEmployeesAsync();
+                _memoryCache.Set("employees", employees, TimeSpan.FromMinutes(1));
+            }
+
+            return employees;
+        }
+    }
+}
